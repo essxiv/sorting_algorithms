30314d3545f9983c5acf199495d491b24c85fbd3
essxiv <essxiv@gmail.com>
2015-10-05 21:21:30 -0700

    fixed quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,20 +1,40 @@
 # Quick Sort
 
-# Using for pivot.
 import random
 
-mylist = (4, 6, 5, 9, 2, 3, 1, 8)
+def quick_sort(mylist):
 
-pivot_index = random.choice(range(len(mylist)))
-pivot = mylist[pivot_index]
-#pivot = random.choice(mylist)
-# assuming unique list
-# pivot_index = mylist.index(pivot)
+    #pivot = random.choice(mylist)
 
+    # left_range = range(0, pivot_index)
+    # right_range = range(pivot_index+1, len(mylist))
+    #pivot_index = mylist.index(pivot)
+    if len(mylist) == 1:
+        return mylist
 
-left_range = range(0, pivot_index)
-right_range = range(pivot_index+1, len(mylist))
+    if len(mylist) == 0:
+        return mylist
 
-left_side = mylist[0:pivot_index]
-right_side = mylist[pivot_index+1:len(mylist)]
+    pivot_index = random.choice(range(len(mylist)))
+    pivot = mylist[pivot_index]
 
+    left_side = mylist[0:pivot_index]
+    right_side = mylist[pivot_index+1:]
+
+    if not left_side and not right_side:
+        return pivot
+
+    right_side_2 = list()
+    left_side_2 = list()
+
+    for item in left_side+right_side:
+        if item > pivot:
+            right_side_2.append(item)
+        if item <= pivot:
+            left_side_2.append(item)
+
+    return (quick_sort(left_side_2) + [pivot] + quick_sort(right_side_2))
+
+mylist = [4, 6, 5, 9, 2, 3, 1, 8]
+print mylist
+print quick_sort(mylist)
